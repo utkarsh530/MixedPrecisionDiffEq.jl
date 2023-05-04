1666ecc5aa4719184ec4032a283e47e8f698bdd3
Utkarsh <rajpututkarsh530@gmail.com>
2023-05-04 18:55:15 -0400

    Add regression tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
 using SafeTestsets, Test
 
-# @time @testset "Regression" begin include("ode_tests.jl") end
+@time @testset "Regression" begin include("ode_tests.jl") end
 @time @testset "Benchmarking" begin include("benchmark.jl") end
